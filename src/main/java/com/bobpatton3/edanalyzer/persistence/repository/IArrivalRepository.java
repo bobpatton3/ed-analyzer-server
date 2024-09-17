acd51c5e6e8a902036bca3fd52ade87ac1dd86a0
bobpatton3 <bobpatton3@gmail.com>
2024-09-17 17:13:46 -0500

    COALESCE to avoid null values for LVL5/CC in FastTrack

--- src/main/java/com/bobpatton3/edanalyzer/persistence/repository/IArrivalRepository.java
+++ src/main/java/com/bobpatton3/edanalyzer/persistence/repository/IArrivalRepository.java
@@ -9,7 +9,7 @@ import com.bobpatton3.edanalyzer.persistence.model.AggregatedArrivalHour;
 
 public interface IArrivalRepository extends CrudRepository<AggregatedArrivalHour, Integer> {
     
-    @Query(value = "SELECT * FROM public.aggregated_arrivals( ?1, ?2, ?3, ?4 )", 
+    @Query(value = "SELECT id, dow, hod, COALESCE(all_avg_rvus, 0.0) AS all_avg_rvus, COALESCE(l5cc_avg_rvus, 0.0) AS l5cc_avg_rvus FROM public.aggregated_arrivals( ?1, ?2, ?3, ?4 )", 
         nativeQuery = true)
     public Iterable<AggregatedArrivalHour> aggregated_arrivals(
         String _start_date,
